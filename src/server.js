01be41b69f3f32b886c7e142cf89153943b9abef
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-06-25 16:08:50 +0200

    Adding initial files

---
+++ src/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const PATH = '/diamond-rings/v1';
+
+// App
+const app = express();
+app.get(PATH, (req, res) => {
+  const ringHeader = req.header('x-ring');
+
+  res.send(`Hello from the ${ringHeader} ring. These are diamond rings.`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
